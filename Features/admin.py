76a64a343ce49147cecafa5ae450b3d2553374f7
Zsa Aupo <zakariasarker407@gmail.com>
2026-05-06 12:55:39 +0600

    Register HeartDiseaseFeature in admin and update database with superuser

--- Features/admin.py
+++ Features/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import HeartDiseaseFeature
 
-# Register your models here.
+@admin.register(HeartDiseaseFeature)
+class HeartDiseaseFeatureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'age', 'sex', 'target', 'max_heart_rate', 'st_depression')
+    list_filter = ('sex', 'target', 'chest_pain_type')
+    search_fields = ('age',)
